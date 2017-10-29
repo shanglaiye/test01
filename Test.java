d1a2539f4ad0561cc17f0c6814a56d720293fbda
Gao Shang <shanglaiye@163.com>
2017-10-29 23:44:17 +0800

    test1
    test1

---
+++ Test.java
@@ -0,0 +1,25 @@
+class PoolPuzzleOne {
+public static void main(String [] args) {
+int x = 0;
+while (  ) {
+
+if ( x < 1 ) {
+
+
+}
+
+
+if ( ) {
+
+}
+if ( x == 1 ) {
+
+}
+if (  ) {
+
+}
+System.out.println(" ");
+
+}
+}
+}
